105b7a85655d10e8d666a90ad02f8c73e48e585c
DarthNaven <Kuehneem@gmail.com>
2025-06-07 09:45:49 -0500

    Finished GRASS.py

---
+++ GRASS.py
@@ -0,0 +1,14 @@
+import pygame
+class Grass:
+    def __init__(self, screen, x, y, image):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("grass.png")
+    def draw (self):
+        self.screen.blit(self.image, (self.x, self.y))
+    def hit_by(self, player):
+        grass_hit_box = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
+        player_hit_box = pygame.Rect(player.x, player.y, player.image.get_width(), player.image.get_height())
+        return grass_hit_box.colliderect(player_hit_box)
+
