b6286482f84499bb98e0d0668eda2b7a65e7b6c5
Carson <ramseycarson2@gmail.com>
2025-06-07 10:33:59 -0400

    Adding to countdown

--- timer.py
+++ timer.py
@@ -4,11 +4,13 @@ import time
 class Timer:
     def __init__(self, screen):
         self.screen = screen
-        self.x = 25
-        self.y = 25
         self.font = pygame.font.SysFont("comicsansms", 20)
         self.start_time = time.time()
 
     def countdown(self):
         time_left = int(60 - (time.time() - self.start_time))
-        print(time_left)
+        if time_left > 10:
+            caption = self.font.render(str(time_left), True, (0, 0, 0))
+        else:
+            caption = self.font.render(str(time_left), True, (255, 0, 0))
+        self.screen.blit(caption, (25, 25))
