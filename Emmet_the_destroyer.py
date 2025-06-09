95ede9672baa5dde5ac4e0b2a5689f397a7a7f90
DarthNaven <Kuehneem@gmail.com>
2025-06-09 12:53:09 -0500

    Finished GRASS.py

--- Emmet_the_destroyer.py
+++ Emmet_the_destroyer.py
@@ -16,7 +16,7 @@ class Rocket:
         self.x = x
         self.y = y
         self.speed = 15
-        self.image = pygame.image.load("stick_character.png")
+        self.image = pygame.image.load("ESTES.png")
         #self.image = pygame.transform.scale(self.image, (30, 30))
 
     def draw(self):
