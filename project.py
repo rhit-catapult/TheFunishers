ee7501432072c0475380e2451bb0398602b8b021
Carson <ramseycarson2@gmail.com>
2025-06-09 11:24:08 -0400

    Got rid of todos

--- project.py
+++ project.py
@@ -14,7 +14,6 @@ def main():
 
     # create a screen
     pygame.display.set_caption("Cool Project")
-    # TODO: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((640, 480))
     # creates a Character from the my_character.py file
     character = my_character.Character(screen, 100, 100)
@@ -31,7 +30,6 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            # TODO: Add you events code
 
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_UP]:
@@ -42,7 +40,6 @@ def main():
             character.x -= 5
         if pressed_keys[pygame.K_RIGHT]:
             character.x += 5
-    # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
         if countdown.countdown():
             end_num = 0
