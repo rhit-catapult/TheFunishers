6cfa256a9b20c28ef9e11342b038b781a430cb8e
DarthNaven <Kuehneem@gmail.com>
2025-06-10 10:18:39 -0500

    I want to be done with ts

--- Emmet_the_destroyer.py
+++ Emmet_the_destroyer.py
@@ -13,16 +13,18 @@ class Emmet:
         self.screen.blit(self.image, (self.x, self.y))
 
     def rain(self):
-        new_rocket = Rocket(self.screen, self.x, self.y + self.image.get_height())
+        new_rocket = Rocket(self.screen, self.x, self.y + self.image.get_height(), (self.image.get_width() / 4), self.image.get_height)
         self.ammo.append(new_rocket)
 class Rocket:
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, x, y, width, length):
         self.screen = screen
         self.x = x
         self.y = y
         self.speed = 15
-        self.image = pygame.draw.line(self.screen, (20,20,20), (self.x, self.y), (self.x, self.y - 10), 5)
-        #self.image = pygame.transform.scale(self.image, (30, 30))
+        self.image = pygame.image.load("ESTES.png")
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.width = width
+        self.length = length
 
     def draw(self):
         self.screen.blit(self.image, (self.x, self.y))
@@ -30,3 +32,18 @@ class Rocket:
     def move(self):
         self.y -= self.speed
 
+def main():
+    pygame.init()
+    pygame.mixer.init()
+    end_font = pygame.font.SysFont("comicsansms", 30)
+
+    pygame.display.set_caption("Cool Project")
+    screen = pygame.display.set_mode((640, 480))
+    death = Emmet(screen, 100, 100)
+    boom = Rocket(screen, 50, 100, 10, 20)
+    while True:
+        screen.fill((160, 160, 160))
+        death.draw()
+        boom.draw()
+        pygame.display.update()
+main()
